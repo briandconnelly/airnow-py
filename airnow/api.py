6397187f9805be5ca220f8db022c7bc40725fb67
Brian Connelly <bconnelly@ookla.com>
2020-09-22 16:29:19 -0700

    Add basic documentation to get_airnow_data

--- airnow/api.py
+++ airnow/api.py
@@ -6,6 +6,11 @@ import requests
 def get_airnow_data(endpoint: str, **kwargs) -> dict:
     """Query the AirNow API and return the contents
 
+    :param str endpoint: AirNow API endpoint (e.g., "/aq/observation/zipCode/current")
+
+    Additional named arguments are passed on as query parameters.
+    All queries should at least have `API_KEY` set.
+    See the AirNow API documentation for parameter lists and descriptions.
     """
 
     with requests.Session() as s:
